f4ecd41634d69dd71a96dfdbd9c379225e075f64
3mpire <charleswesley@gmail.com>
2014-11-21 21:42:51 -0800

    renamed css and js folders and updated src/href values in index.html

---
+++ js/clock.js
@@ -0,0 +1,61 @@
+$(document).ready(function () {
+    startClock();
+});
+
+function startClock() {
+    'use strict';
+    var delay = 1000;
+
+    function tick() {
+        var clockDate = new Date(),
+            clockHours = clockDate.getHours(),
+            clockMinutes = clockDate.getMinutes(),
+            fiveHours = Math.floor(clockHours / 5),
+            fiveMinutes = Math.floor(clockMinutes / 5);
+
+        $("#0").toggleClass("off");
+        $("#display").text(clockHours + ":" + ((clockMinutes < 10) ? "0" + clockMinutes : clockMinutes));
+        
+        $("[id^=h]").addClass("off");
+        $("[id^=m]").addClass("off");
+        
+        // First row of lamps
+        if (fiveHours >= 1) {
+            for (var i = 1; i <= fiveHours; i++) {
+                $("#h5-" + i).removeClass("off");
+            }
+        }
+
+        // Second row of lamps
+        if (fiveHours % 5 > 0) {
+            var singleHours = Math.floor(clockHours - (Math.floor(fiveHours) * 5));
+
+            for (var i = 1; i <= singleHours; i++) {
+                $("#h1-" + i).removeClass("off");
+            }
+        }
+
+        // Third row of lamps
+        if (fiveMinutes >= 1) {
+            for (var i = 1; i <= fiveMinutes; i++) {
+                if (i % 3 == 0) {
+                    $("#m5-" + i).removeClass("off");
+                } else {
+                    $("#m5-" + i).removeClass("off");
+                }
+            }
+        }
+
+        // Fourth row of lamps
+        if (fiveMinutes % 5 > 0) {
+            var singleMinutes = Math.floor(clockMinutes - (Math.floor(fiveMinutes) * 5));
+            for (var i = 1; i <= singleMinutes; i++) {
+                $("#m1-" + i).removeClass("off");
+            }
+        }
+
+        setTimeout(tick, delay);
+    }
+
+    tick();
+}
